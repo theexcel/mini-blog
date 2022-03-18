6ff54bc253637dd1e37bc7a07a17a7f475101e53
Excel Olatunbosun <xclolatunbosun@gmail.com>
2022-03-18 15:13:17 +0100

    setting up environment

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from 'react-navigation-stack'
+import IndexScreen from './src/screen/indexScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const navigator = createStackNavigator({
+  Index: IndexScreen
+}, {
+  initialRouteName: 'Index', 
+  defaultNavigationOptions: {
+    title: 'Blogs'
+  }
+})
---
+++ src/screen/IndexScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, StyleSheet, text } from "react-native";
+
+const IndexScreen = () => {
+  return (
+    <View>
+      <Text>Index Screen</Text>
+    </View>
+  );
+};
+
+
+const styles = StyleSheet.create({});
+
+export default IndexScreen;
