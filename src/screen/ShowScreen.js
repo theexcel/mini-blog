bc844aa0b4f8658381928ecc1de26d0b7c4ab41f
Excel Olatunbosun <xclolatunbosun@gmail.com>
2022-03-20 13:42:41 +0100

    creating a new screen to show blog posts when a user taps a blog post

---
+++ src/screen/ShowScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const ShowScreen = () => {
+
+    return (
+        <View>
+            <Text>Show Show Screen</Text>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+
+})
+
+export default ShowScreen
