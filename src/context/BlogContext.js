7273c2b6729f760c747a2d1a88c9de57fb83f2f7
Excel Olatunbosun <xclolatunbosun@gmail.com>
2022-03-20 07:49:53 +0100

    automating context

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,6 +1,5 @@
 import React, { useReducer } from "react";
-
-const BlogContext = React.createContext();
+import createDataContext from "./createDataContext";
 
 const blogReducer = (state, action) => {
   switch (action.type) {
@@ -11,18 +10,12 @@ const blogReducer = (state, action) => {
   }
 };
 
-export const BlogProvider = ({ children }) => {
-  const [blogPosts, dispatch] = useReducer(blogReducer, []);
-
 const addBlogPost = () => {
-    dispatch({type: 'add_blogpost'})
-}
-
-  return (
-    <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
-      {children}
-    </BlogContext.Provider>
-  );
+  dispatch({ type: "add_blogpost" });
 };
 
-export default BlogContext;
+export const { Context, Provider } = createDataContext(
+  blogReducer,
+  { addBlogPost },
+  []
+);
