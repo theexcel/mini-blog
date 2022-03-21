31a3f67b0bfab329574ac64f7583e57cab7463ea
Excel Olatunbosun <xclolatunbosun@gmail.com>
2022-03-21 11:36:18 +0100

    edit feature implemented

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -2,6 +2,10 @@ import createDataContext from "./createDataContext";
 
 const blogReducer = (state, action) => {
   switch (action.type) {
+    case "edit_blogpost":
+      return state.map((blogPost) => {
+        return blogPost.id === action.payload.id ? action.payload : blogPost;
+      });
     case "delete_blogpost":
       return state.filter((blogPost) => blogPost.id !== action.payload);
     case "add_blogpost":
@@ -10,7 +14,7 @@ const blogReducer = (state, action) => {
         {
           id: Math.floor(Math.random() * 99999),
           title: action.payload.title,
-          content: action.payload.content
+          content: action.payload.content,
         },
       ];
     default:
@@ -20,8 +24,10 @@ const blogReducer = (state, action) => {
 
 const addBlogPost = (dispatch) => {
   return (title, content, callback) => {
-    dispatch({ type: "add_blogpost", payload: {title, content} });
-    callback();
+    dispatch({ type: "add_blogpost", payload: { title, content } });
+    if(callback){
+        callback();
+    }
   };
 };
 const deleteBlogPost = (dispatch) => {
@@ -29,9 +35,17 @@ const deleteBlogPost = (dispatch) => {
     dispatch({ type: "delete_blogpost", payload: id });
   };
 };
+const editBlogPost = (dispatch) => {
+  return (id, title, content, callback) => {
+    dispatch({ type: "edit_blogpost", payload: { id, title, content } });
+    if(callback){
+        callback();
+    }
+  };
+};
 
 export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPost, deleteBlogPost },
-  [{title: 'Test Post', content: 'Test Content', id: 1}]
+  { addBlogPost, deleteBlogPost, editBlogPost },
+  [{ title: "Test Post", content: "Test Content", id: 1 }]
 );
--- src/screen/EditScreen.js
+++ src/screen/EditScreen.js
@@ -1,25 +1,24 @@
-import React, {useState, useContext} from 'react';
-import {View, Text, Button, StyleSheet, TextInput} from 'react-native'
-import {Context} from '../context/BlogContext'
+import React, { useState, useContext } from "react";
+import { View, Text, Button, StyleSheet, TextInput } from "react-native";
+import { Context } from "../context/BlogContext";
+import BlogPostForm from "../components/BlogPostForm";
 
-const EditScreen = ({navigation}) => {
-const {state} = useContext(Context)
+const EditScreen = ({ navigation }) => {
+  const id = navigation.getParam("id");
+  const { state, editBlogPost } = useContext(Context);
 
-const blogPost = state.find(blogPost => blogPost.id === navigation.getParam('id'))
+  const blogPost = state.find((blogPost) => blogPost.id === id);
 
+  return (
+    <BlogPostForm
+      initialValues={{ title: blogPost.title, content: blogPost.content }}
+      onSubmit={(title, content) => {
+        editBlogPost(id, title, content, () => navigation.pop());
+      }}
+    />
+  );
+};
 
-const [title, setTitle] = useState(blogPost.title)
-const [content, setContent]  = useState(blogPost.content)
-    return (
-        <View>
-            <Text>Edit Title</Text>
-            <TextInput value = {title} onChangeText = {(newTitle) => setTitle(newTitle)}/>
-        </View>
-    )
-}
+const styles = StyleSheet.create({});
 
-const styles = StyleSheet.create({
-
-})
-
-export default EditScreen
+export default EditScreen;
